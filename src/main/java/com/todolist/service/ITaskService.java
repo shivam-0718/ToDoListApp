ef963c8ec5ebd1ca6c81c7ddff0c718cd18260e1
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-08-15 22:06:37 +0530

    Created ITaskService and it's implementation class for Service layer.

---
+++ src/main/java/com/todolist/service/ITaskService.java
@@ -0,0 +1,9 @@
+package com.todolist.service;
+
+import com.todolist.model.Task;
+
+import java.util.List;
+
+public interface ITaskService {
+    List<Task> getAllTasks();
+}
---
+++ src/main/java/com/todolist/service/TaskService.java
@@ -0,0 +1,23 @@
+package com.todolist.service;
+
+import com.todolist.model.Task;
+import com.todolist.repo.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class TaskService implements ITaskService {
+
+    private final TaskRepository taskRepo;
+
+    @Autowired
+    public TaskService(TaskRepository taskRepo) {
+        this.taskRepo = taskRepo;
+    }
+
+    @Override
+    public List<Task> getAllTasks() {
+        return List.of();
+    }
+}
