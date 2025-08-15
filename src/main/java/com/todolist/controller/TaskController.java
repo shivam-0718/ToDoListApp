70cd06be907bb9236a2cf4c324a4109729f27020
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-08-15 22:06:51 +0530

    Created TaskController for controller layer.

---
+++ src/main/java/com/todolist/controller/TaskController.java
@@ -0,0 +1,30 @@
+package com.todolist.controller;
+
+import com.todolist.model.Task;
+import com.todolist.service.ITaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("api/v1")
+public class TaskController {
+
+    private final ITaskService service;
+
+    @Autowired
+    public TaskController(ITaskService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public String getTasks(Model model) {
+        List<Task> tasks = service.getAllTasks();
+        model.addAttribute("tasks", tasks);
+        return "tasks";
+    }
+}
