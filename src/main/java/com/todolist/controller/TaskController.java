de32f6cd0df2e04aa76fc19adc596ccf2461eabd
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-08-15 22:46:16 +0530

    Added logic of adding a task and home page.

--- src/main/java/com/todolist/controller/TaskController.java
+++ src/main/java/com/todolist/controller/TaskController.java
@@ -6,11 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 
 @Controller
-@RequestMapping("v1")
 public class TaskController {
 
     private final ITaskService service;
@@ -20,10 +20,21 @@ public class TaskController {
         this.service = service;
     }
 
+    @GetMapping({"/", ""})
+    public String home() {
+        return "redirect:/tasks";
+    }
+
     @GetMapping("tasks")
     public String getTasks(Model model) {
         List<Task> tasks = service.getAllTasks();
         model.addAttribute("tasks", tasks);
         return "tasks";
+    }
+
+    @PostMapping("new-task")
+    public String addTask(@RequestParam String title) {
+        service.createTask(title);
+        return "redirect:/tasks";
     }
 }
