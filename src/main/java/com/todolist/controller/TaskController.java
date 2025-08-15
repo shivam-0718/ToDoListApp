dd6b36bf55f8fed91eef8292acfab29ca6dc587d
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-08-15 22:17:12 +0530

    Added getTasks().

--- src/main/java/com/todolist/controller/TaskController.java
+++ src/main/java/com/todolist/controller/TaskController.java
@@ -7,11 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import java.util.List;
 
 @Controller
-@RequestMapping("api/v1")
+@RequestMapping("v1")
 public class TaskController {
 
     private final ITaskService service;
@@ -21,7 +20,7 @@ public class TaskController {
         this.service = service;
     }
 
-    @GetMapping
+    @GetMapping("tasks")
     public String getTasks(Model model) {
         List<Task> tasks = service.getAllTasks();
         model.addAttribute("tasks", tasks);
